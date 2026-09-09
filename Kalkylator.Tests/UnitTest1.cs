3c3fe74f02d0f2a9af83d70cd39399379909d2e3
peteromneus <271960984+peteromneus@users.noreply.github.com>
2026-09-09 17:08:23 +0200

    add faulty expectation

--- Kalkylator.Tests/UnitTest1.cs
+++ Kalkylator.Tests/UnitTest1.cs
@@ -6,6 +6,6 @@ public class RaknareTests
     public void AdderaTvaPlusTreBlirFem()
     {
         var raknare = new Raknare();
-        Assert.Equal(5, raknare.Addera(2, 3));
+        Assert.Equal(6, raknare.Addera(2, 3));
     }
 }
